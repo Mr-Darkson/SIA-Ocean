727d9394c1bca924d3c4460c3df53b4bac477fc3
Mr-Darkson <129211480+Mr-Darkson@users.noreply.github.com>
2023-04-22 17:02:50 +0300

    Add files via upload

---
+++ Engine.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "Constants.h"
+#include "Environment.h"
+
+void fillZero(OceanCell ocean[Y_SIZE][X_SIZE]) {
+	for (int y = 0; y < Y_SIZE; y++) {
+		for (int x = 0; x < X_SIZE; x++) {
+			ocean[y][x].symbol = VOID;
+		}
+	}
+}
+
+void printOcean(OceanCell ocean[Y_SIZE][X_SIZE]) {
+	for (int y = 0; y < Y_SIZE; y++) {
+		for (int x = 0; x < X_SIZE; x++) {
+			printf("%c", ocean[y][x].symbol);
+		}
+		printf("\n");
+	}
+}
